2cc972b6c23eb64cb8098a7adec7503b8915d704
Victory <uvictory25@gmail.com>
2024-06-19 06:18:32 +0200

    Adding vendor for dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/gobuy_encryption',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd200e584f893ae279eda7d4950ca998130d857b9',
+        'type' => 'false',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/gobuy_encryption' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd200e584f893ae279eda7d4950ca998130d857b9',
+            'type' => 'false',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
